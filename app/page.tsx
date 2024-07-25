308468a184370947052e9d72c3021796691ff74d
massu-159 <magjtwjp@gmail.com>
2024-07-25 20:18:00 +0900

    update page

--- app/page.tsx
+++ app/page.tsx
@@ -23,6 +23,7 @@ export default function Home() {
           <li>Save and see your changes instantly.</li>
           <li>2回目の編集</li>
           <li>3回目の編集</li>
+          <li>4回目の編集</li>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
